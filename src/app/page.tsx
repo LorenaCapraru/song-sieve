2e595612c3e5830fa5bd3f40d02f53b69c12bccc
Mo Mahboobian <momahboobian@gmail.com>
2023-11-03 14:34:10 +0000

    Adding firebase configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  FacebookAuthProvider,
+} from "firebase/auth";
+import { auth } from "@/firebase/firebase";
+
 export default function Home() {
-  return <div>Home</div>;
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+
+    try {
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+      if (user && user.email) {
+        const email = user.email;
+        console.log("Signed in with email:", email);
+      }
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      console.error("Error signing in with Google:", errorCode, errorMessage);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleGoogleSignIn}>Sign in google</button>
+
+      <button>Sign up</button>
+    </div>
+  );
 }

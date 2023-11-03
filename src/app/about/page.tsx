b69b43b67376a58d414bc591532bb41856113dee
Mo Mahboobian <momahboobian@gmail.com>
2023-11-03 14:57:58 +0000

    Adding sample file and folder

---
+++ src/app/about/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  FacebookAuthProvider,
+} from "firebase/auth";
+import { auth } from "@/firebase/firebase";
+
+export default function Home() {
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+
+    try {
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+      if (user && user.email) {
+        const email = user.email;
+        console.log("Signed in with email:", email);
+      }
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      console.error("Error signing in with Google:", errorCode, errorMessage);
+    }
+  };
+
+  return (
+    <div>
+      About
+      {/* <button onClick={handleGoogleSignIn}>Sign in google</button>
+
+      <button>Sign up</button> */}
+    </div>
+  );
+}

9e5093a7b121ffd8fed54f8966a35d7acefdb27f
Lorena <lorenacapraru@gmail.com>
2023-11-07 20:02:44 +0000

    Add the heading and description

--- src/app/components/CheckPlaylist/CheckPlaylist.tsx
+++ src/app/components/CheckPlaylist/CheckPlaylist.tsx
@@ -1,7 +1,16 @@
+import "./CheckPlaylist.css";
+
 const CheckPlaylist = () => {
   return (
     <div className="check-playlist-section">
-      <h1> Check the playlist from your friend</h1>
+      <h1 className="check-playlist-header">
+        {" "}
+        Check the playlist from your friend
+      </h1>
+      <div className="id-provide-section">
+        <p>Provide id of Spotify playlist</p>
+        <p>place for input</p>
+      </div>
     </div>
   );
 };

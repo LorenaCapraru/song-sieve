939e079bd1744686f74c7375fb846b7cc63a5e0c
Lorena <lorenacapraru@gmail.com>
2023-11-07 23:25:52 +0000

    Brought Input changes into Input component

--- src/app/components/CheckPlaylist/CheckPlaylist.tsx
+++ src/app/components/CheckPlaylist/CheckPlaylist.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Input from "../Input/Input";
 import "./CheckPlaylist.css";
 
--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -1,22 +1,40 @@
 import Image from "next/image";
+import { useState, ChangeEvent } from "react";
 import "./Input.css";
+
 const Input = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <div className="input-section">
-      <Image
-        src="/icons/spotify-icon.svg"
-        alt="spotify brand"
-        width={16}
-        height={16}
-        className="icon"
-      />
-      <input
-        type="text"
-        value=""
-        placeholder="Spotify playlist id"
-        className="input-field"
-      />
-    </div>
+    <>
+      <div className="input-section">
+        <Image
+          src="/icons/spotify-icon.svg"
+          alt="spotify brand"
+          width={16}
+          height={16}
+          className="icon"
+        />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Spotify playlist id"
+          className="input-field"
+        />
+      </div>
+      <div className="submit-button-section">
+        <input
+          className="submit-button"
+          type="submit"
+          value="GET THE PLAYLIST"
+        />
+      </div>
+    </>
   );
 };
 

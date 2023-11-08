05b5b0dd65ab4c38d9b9c79667af0bce68f251c7
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-11-08 20:32:08 +0000

    Change folder structure and modify css of submit-button in Input.tsx

--- src/app/components/CheckPlaylist/CheckPlaylist.tsx
+++ src/app/components/CheckPlaylist/CheckPlaylist.tsx
@@ -1,13 +1,12 @@
 "use client";
-import Input from "../Input/Input";
-import Playlist from "../Playlist/Playlist";
+import Input from "./components/Input/Input";
+import Playlist from "./components/Playlist/Playlist";
 import "./CheckPlaylist.css";
 
 const CheckPlaylist = () => {
   return (
     <div className="check-playlist-section">
       <h1 className="check-playlist-header">
-        {" "}
         Check the playlist from your friend
       </h1>
       <div className="id-provide-section">

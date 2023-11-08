d516cf8a8d98870cdd8f9cb72972390159f6532f
Lorena <lorenacapraru@gmail.com>
2023-11-08 00:13:07 +0000

    created states for playlist rendering

--- src/app/components/Playlist/Playlist.tsx
+++ src/app/components/Playlist/Playlist.tsx
@@ -1,22 +1,40 @@
 import "./Playlist.css";
 import Image from "next/image";
+import { useState } from "react";
 
 const Playlist = () => {
+  const [playlistId, setPlayListId] = useState<string>("hfjdkvfhfolskdjfhf");
+  const [tracks, setTracks] = useState<number>(25);
+  const [description, setDescription] = useState<string>(
+    "Kick back to the best new and recent chill hits"
+  );
+  const [playListName, setPlayListName] = useState<string>("Chill hits");
+  const [coverImage, setCoverImage] = useState<string>(
+    "/background_images/background_2.jpg"
+  );
+
   return (
     <div className="playlist-section">
-      <Image
-        src="/background_images/background_2.jpg"
-        alt="album cover"
-        width={87}
-        height={80}
-        className="album-cover"
-      />
-      <div className="song-details">details</div>
+      <div className="playlist-song-and-description">
+        <Image
+          src={coverImage}
+          alt="album cover"
+          width={87}
+          height={80}
+          className="album-cover"
+        />
+        <div className="playlist-brief">
+          <p className="playlist-name">Chill hits</p>
+          <p className="detail">{description} </p>
+          <p className="detail">id: {playlistId}</p>
+          <p className="detail">tracks:{tracks}</p>
+        </div>
+      </div>
       <Image
         src="/icons/heart-icon.svg"
         alt="heart icon to save"
-        width={25}
-        height={25}
+        width={15}
+        height={15}
         className="heart-save"
       />
     </div>

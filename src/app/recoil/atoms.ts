8e969c64f82de6ae6183e27c90e4da425731561e
Lorena <lorenacapraru@gmail.com>
2023-11-13 15:12:31 +0000

    Cleaned unnecessary states

--- src/app/recoil/atoms.ts
+++ src/app/recoil/atoms.ts
@@ -25,30 +25,10 @@ export const inputSpotifyLinkState = atom<string>({
 //Playlist.tsx states for rendering data about a Playlist
 export const playlistIdState = atom<string>({
   key: "playlistIdState",
-  default: "hfjdkvfhfolskdjfhf",
+  default: "",
 });
 
 export const playlistDataState = atom<object>({
   key: "playlistDataState",
   default: {},
-});
-
-export const tracksState = atom<number>({
-  key: "tracksState",
-  default: 25,
-});
-
-export const descriptionState = atom<string>({
-  key: "descriptionState",
-  default: "Kick back to the best new and recent chill hits",
-});
-
-export const playListNameState = atom<string>({
-  key: "playListNameState",
-  default: "Chill hits",
-});
-
-export const coverImageState = atom<string>({
-  key: "coverImageState",
-  default: "/background_images/background_2.jpg",
 });

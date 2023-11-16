a45d3f11747a63f1b5d85ba4841e04835df3c8a5
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-11-16 11:51:03 +0000

    Add MainPlaylist component to it

---
+++ src/app/playlist/components/MainPlaylist/MainPlaylist.tsx
@@ -0,0 +1,9 @@
+import "./MainPlaylist.css";
+
+const MainPlaylist = () => {
+  return (
+    <div>here we put everything what should be from the right side in Main</div>
+  );
+};
+
+export default MainPlaylist;

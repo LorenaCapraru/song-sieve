ec53f991bdb6faead4f1cf8d9312e9e45a82dc26
Lorena <lorenacapraru@gmail.com>
2023-11-16 12:44:06 +0000

    created the skeleton of buttons/data to display

--- src/app/playlist/components/PlaylistHeader/PlaylistHeader.tsx
+++ src/app/playlist/components/PlaylistHeader/PlaylistHeader.tsx
@@ -1,5 +1,27 @@
+"use client";
+import { useRecoilValue } from "recoil";
+import "./PlaylistHeader.css";
+import { playlistDataState, PlaylistData } from "@/app/recoil/atoms";
+
 const PlaylistHeader = () => {
-  return <div>playlist header</div>;
+  // console.log()
+  const playlistData = useRecoilValue<PlaylistData | undefined>(
+    playlistDataState
+  );
+  console.log(playlistData);
+  return (
+    playlistData && (
+      <div>
+        <div className="playlist-header-data">
+          <p>Playlist</p>
+          <p>{playlistData?.name}</p>
+          <p>{playlistData?.tracks.total} songs</p>
+          <p>{playlistData?.id}</p>
+        </div>
+        <div className="playlist-header-buttons">button</div>
+      </div>
+    )
+  );
 };
 
 export default PlaylistHeader;

21047689ea225a6019dfac1cdbac2812f35d03ae
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-11-19 19:53:59 +0000

    Add background images on load to every page

--- src/app/playlist/[id]/page.tsx
+++ src/app/playlist/[id]/page.tsx
@@ -1,11 +1,18 @@
+"use client";
 import Header from "@/app/components/Header/Header";
 import "./page.css";
 import SideBar from "@/app/components/SideBar/SideBar";
 import MainPlaylist from "../components/MainPlaylist/MainPlaylist";
 import MobileMenu from "../../components/MobileMenu/MobileMenu";
+import { useEffect } from "react";
 
 const PlaylistPage = ({ params }: { params: { id: string } }) => {
   console.log("id", params.id);
+
+  //update background image on first load
+  useEffect(() => {
+    document.body.style.backgroundImage = "url(/background_images/back_2.webp)";
+  }, []);
 
   return (
     <div className="bg-template home-template">

f67a658d7ac5f96010ab19d8e587a210ffc27269
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-11-19 21:50:32 +0000

    Fetch list of tracks when the playlistData is undefined

--- src/app/playlist/[id]/page.tsx
+++ src/app/playlist/[id]/page.tsx
@@ -5,13 +5,46 @@ import SideBar from "@/app/components/SideBar/SideBar";
 import MainPlaylist from "../components/MainPlaylist/MainPlaylist";
 import MobileMenu from "../../components/MobileMenu/MobileMenu";
 import { useEffect } from "react";
+import { checkTokenTime } from "@/utils/utils";
+import { useRecoilState } from "recoil";
+import { playlistDataState } from "@/app/recoil/atoms";
 
 const PlaylistPage = ({ params }: { params: { id: string } }) => {
   console.log("id", params.id);
+  const [playlistData, setPlaylistData] = useRecoilState(playlistDataState);
 
-  //update background image on first load
+  //update background image on first load and fetch the playlist data if it's needed
   useEffect(() => {
     document.body.style.backgroundImage = "url(/background_images/back_2.webp)";
+
+    const fetchPlaylist = async () => {
+      console.log("getch data");
+      await checkTokenTime();
+      const accessToken = localStorage.getItem("access_token");
+
+      try {
+        const response = await fetch(
+          `https://api.spotify.com/v1/playlists/${params.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`Error fetching playlist: ${response.status}`);
+        }
+        const data = await response.json();
+        setPlaylistData(data);
+        console.log("Playlist Data:", data);
+      } catch (error) {
+        console.error("Error fetching playlist" + error);
+      }
+    };
+
+    if (!playlistData) {
+      fetchPlaylist();
+    }
   }, []);
 
   return (

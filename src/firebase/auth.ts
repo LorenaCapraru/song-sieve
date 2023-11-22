e4cf6cfe5b99aa36721d0629140699ff2fbcddea
Mo Mahboobian <momahboobian@gmail.com>
2023-11-22 13:00:13 +0000

    Implementing the signUp auth.ts for signUp page

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,3 +1,28 @@
-export default function auth() {
-  return <div>auth</div>;
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  Auth,
+  UserCredential,
+} from "firebase/auth";
+import { SignUpState } from "../app/recoil/atoms";
+
+const authInstance: Auth = getAuth(); // Initialize Firebase auth instance
+
+// Sign Up
+export async function signUpUser(
+  authData: SignUpState
+): Promise<UserCredential> {
+  const { email, password } = authData;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      authInstance,
+      email,
+      password
+    );
+    return userCredential;
+  } catch (error: any) {
+    console.error("Error signing up:", error.message);
+    throw error;
+  }
 }

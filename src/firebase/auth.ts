98643f29ab32c907ff5c2bb69b369a64b6a84f5a
Mo Mahboobian <momahboobian@gmail.com>
2023-11-22 14:09:29 +0000

    Updatinf the auth file and sign Up page implementation

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,68 +1,31 @@
 import {
-  Auth,
   getAuth,
+  createUserWithEmailAndPassword,
+  Auth,
   UserCredential,
-  signInWithEmailAndPassword as signIn,
 } from "firebase/auth";
-
+import { SignUpState } from "../app/recoil/atoms";
 import { auth } from "./firebase";
 
-const app = initializeApp(firebaseConfig);
-const auth: Auth = getAuth(app);
+const authInstance: Auth = auth;
 
 // Sign Up
-// export const signUpWithEmailAndPassword = async (
-//   email: string,
-//   password: string
-// ) => {
-//   try {
-//     const userCredential = await createUserWithEmailAndPassword(
-//       auth,
-//       email,
-//       password
-//     );
-//     // Additional logic after successful sign-up
-//     return userCredential.user;
-//   } catch (error: any) {
-//     console.error("Error signing up:", error.message);
-//     throw error;
-//   }
-// };
+export async function signUpUser(
+  authData: SignUpState
+): Promise<UserCredential> {
+  const { email, password } = authData;
 
-// Sign In
-export const signInWithEmail = async (email: string, password: string) => {
   try {
-    const userCredential = await auth.signInWithEmailAndPassword(
+    const userCredential = await createUserWithEmailAndPassword(
+      authInstance,
       email,
       password
     );
-    return userCredential.user;
+    return userCredential;
   } catch (error: any) {
     console.error("Error signing up:", error.message);
     throw error;
   }
-};
-/*
-// Sign Out
-export const signOut = async () => {
-  try {
-    await auth.signOut();
-  } catch (error) {
-    throw error;
-  }
-};
+}
 
-// Get Current User
-export const getCurrentUser = () => {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = onAuthStateChanged(
-      auth,
-      user => {
-        unsubscribe();
-        resolve(user);
-      },
-      reject
-    );
-  });
-};
-*/
+// Sign In

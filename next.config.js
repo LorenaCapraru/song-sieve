761ddf229a05b7db9112805ce27140dff86ef250
Lorena <lorenacapraru@gmail.com>
2023-11-23 11:47:43 +0000

    removed export timeout

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    process.env.NEXT_EXPORT_TIMEOUT = "300";
-    return defaultPathMap;
-  },
   images: {
     remotePatterns: [
       {

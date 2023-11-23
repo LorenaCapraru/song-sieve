ee44137c403692b8868896f79b437b621b0fe315
Lorena <lorenacapraru@gmail.com>
2023-11-23 11:31:13 +0000

    Increase timeout to deploy successfully

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    process.env.NEXT_EXPORT_TIMEOUT = "300";
+    return defaultPathMap;
+  },
   images: {
     remotePatterns: [
       {

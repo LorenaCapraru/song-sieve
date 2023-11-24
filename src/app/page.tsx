3520c859a9d9d2ef8ab76657f0b0536d7d883777
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-11-24 22:59:19 +0000

    Fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,17 @@ import MobileMenu from "./components/MobileMenu/MobileMenu";
 import SideBar from "./components/SideBar/SideBar";
 import PopupLogin from "./components/PopupLogIn/PopupLogin";
 import PopupConfirm from "./playlist/components/Track/components/PopupConfirm/PopupConfirm";
+import { useSetRecoilState } from "recoil";
+import { isFavouriteTracksPageState } from "./recoil/atoms";
 
 export default function Home() {
+  const setIsFavouriteTracksPage = useSetRecoilState<boolean>(
+    isFavouriteTracksPageState
+  );
   //update background image on first load
   useEffect(() => {
     document.body.style.backgroundImage = "url(/background_images/back_1.webp)";
+    setIsFavouriteTracksPage(false);
   }, []);
 
   return (
